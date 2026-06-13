3bb3df1d6edb9922a9fd0e81b1810dedb7549b3e
“NotBenthe” <138243966+NotBenthe@users.noreply.github.com>
2026-06-13 17:22:03 +0200

    art edit

--- pygame_tutorial.py
+++ pygame_tutorial.py
@@ -35,6 +35,7 @@ SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 600
 SPEED = 25
 SCORE = 0
+MAX_ENEMIES = 3
 
 # setting up fonts
 font = pygame.font.SysFont("Verdana", 40)
@@ -52,7 +53,7 @@ DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("pygame tutorial")
 
 # setting the background
-background = pygame.image.load("AnimatedStreet.png")
+background = pygame.image.load("background.png")
 background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT))
 background_y = 0  # tracking vertical position
 
@@ -68,13 +69,13 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
 
         # loading image
-        self.image = pygame.image.load("Player.png")
+        self.image = pygame.image.load("player.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, (100, 200))
 
         # creating rectangular hitbox
         self.rect = self.image.get_rect()
-        self.rect.center = (SCREEN_WIDTH/2, SCREEN_HEIGHT-80)
-        self.mask = pygame.mask.from_surface(pygame.transform.scale(self.image, (65, 160)))
+        self.rect.center = (SCREEN_WIDTH/2, SCREEN_HEIGHT-200)
+        self.mask = pygame.mask.from_surface(self.image)#pygame.transform.scale(self.image, (65, 160)))
 
     def move(self):
         pressed_keys = pygame.key.get_pressed()
@@ -83,7 +84,7 @@ class Player(pygame.sprite.Sprite):
         if self.rect.top > 0:
             if pressed_keys[K_UP]:
                 self.rect.move_ip(0, -1 * SPEED)
-        if self.rect.bottom < SCREEN_WIDTH:
+        if self.rect.bottom < SCREEN_HEIGHT:
             if pressed_keys[K_DOWN]:
                 self.rect.move_ip(0, SPEED)
 
@@ -102,37 +103,49 @@ class Enemy(pygame.sprite.Sprite):
         # initiating super
         super().__init__()
 
-        self.image = pygame.image.load("Enemy.png")
-        self.image = pygame.transform.scale(self.image, (100, 200))
+        self.image = pygame.image.load("enemy.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (50, 200))
 
         self.rect = self.image.get_rect()
-        self.rect.center = (random.randint(40, SCREEN_WIDTH-40), 0)
-        self.mask = pygame.mask.from_surface(pygame.transform.scale(self.image, (65, 160)))
+        self.rect.center = (random.randint(40, SCREEN_WIDTH-40), -200)
+        self.mask = pygame.mask.from_surface(self.image)#pygame.transform.scale(self.image, (100, 200)))
+
+        # making sure the enemies don't overlap 
+        valid_position = False 
+        while not valid_position:
+            self.rect.center = (random.randint(50, SCREEN_WIDTH - 50), random.randint(-400, -200))
+            if not pygame.sprite.spritecollideany(self, enemies):
+                valid_position = True 
 
     def move(self):
         global SCORE
         self.rect.move_ip(0, SPEED)
         if (self.rect.bottom > SCREEN_HEIGHT):
             SCORE += 1
-            self.rect.top = 0
-            self.rect.center = (random.randint(40, SCREEN_WIDTH - 80), 0)
+            #self.rect.top = 0
+            #self.rect.center = (random.randint(40, SCREEN_WIDTH - 80), 0)
+            self.kill()
 
 
 # initiating enemy and player
-E1 = Enemy()
 P1 = Player()
 
 # creating sprites group
 enemies = pygame.sprite.Group()
-enemies.add(E1)
+
 all_sprites = pygame.sprite.Group()
 all_sprites.add(P1)
-all_sprites.add(E1)
+
 
 # creating event
 INC_SPEED = pygame.USEREVENT + 1 
 # call the INC_SPEED object every 1000 ms (=1s)
 pygame.time.set_timer(INC_SPEED, 2000)
+
+# spawning enemies
+SPAWN_ENEMY = pygame.USEREVENT + 2
+# trying to spawn a new enemy every 1-5 seconds
+pygame.time.set_timer(SPAWN_ENEMY, random.randint(1000, 3000))
 
 # running the game
 while True: 
@@ -141,6 +154,15 @@ while True:
         # increasing the speed (every 1 s)
         if event.type == INC_SPEED:
             SPEED += 2
+        
+        # spawning enemies
+        if event.type == SPAWN_ENEMY:
+            if len(enemies) < MAX_ENEMIES:
+                E = Enemy()
+                enemies.add(E)
+                all_sprites.add(E)
+
+            pygame.time.set_timer(SPAWN_ENEMY, random.randint(int(10000/SPEED), int(30000/SPEED)))
 
         # quitting
         if event.type == QUIT:
@@ -151,7 +173,7 @@ while True:
     DISPLAYSURF.fill(WHITE)
 
     # moving the background 
-    background_y += 0.5 * SPEED 
+    background_y += 5
     if background_y >= SCREEN_HEIGHT:
         background_y = 0
 
@@ -159,7 +181,7 @@ while True:
     DISPLAYSURF.blit(background, (0, background_y))
     DISPLAYSURF.blit(background, (0, background_y - SCREEN_HEIGHT))
 
-    scores = font_small.render(str(SCORE), True, BLACK)
+    scores = font_small.render(str(SCORE), True, GREEN)
     DISPLAYSURF.blit(scores, (40, 40))
 
     # drawing the entities
@@ -168,15 +190,18 @@ while True:
         entity.move()
         
     # collision
-    if pygame.sprite.spritecollide(P1, enemies, False, pygame.sprite.collide_mask):
+    if pygame.sprite.spritecollide(P1, enemies, False, pygame.sprite.collide_mask) :
         # playing crashing sound
         pygame.mixer.Sound('crash.wav').play()
-        time.sleep(0.5)
+        #time.sleep(0.5)
 
         # making screen red
         DISPLAYSURF.fill(RED)
-        DISPLAYSURF.blit(scores, (10, 10))
-        DISPLAYSURF.blit(game_over, (30, 250))
+        #DISPLAYSURF.blit(scores, (40, 40))
+        DISPLAYSURF.blit(game_over, (SCREEN_WIDTH/2-200, SCREEN_HEIGHT/2))
+        score_go = font.render(f"Score: {str(SCORE)}", True, BLACK)
+        DISPLAYSURF.blit(score_go, (SCREEN_WIDTH/2-200, 3* SCREEN_HEIGHT/4))
+
         pygame.display.update()
 
         # killing all the sprites
